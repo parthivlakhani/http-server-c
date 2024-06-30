27b5b9dd70e03ff3a074c576d77221020f836ba0
parthivlakhani <567parthiv@gmail.com>
2024-06-30 12:51:09 +0530

    pass stage

--- app/server.c
+++ app/server.c
@@ -97,15 +97,14 @@ void handle_connection(int fd){
     	return;
   	}	
 	req_buffer[bytesReceived] = '\0';
+	
+	char *method = strdup(req_buffer);
+	char *content = strdup(req_buffer);
+	printf("Content: %s\n", content);
+	method = strtok(method, " ");
+	char *reqpath = strtok(req_buffer, " ");
+	reqpath = strtok(NULL, " ");
 
-	// char *method = strdup(req_buffer);
-	// char *content = strdup(req_buffer);
-	// printf("Content: %s\n", content);
-	// method = strtok(method, " ");
-	// char *reqpath = strtok(req_buffer, " ");
-	// reqpath = strtok(NULL, " ");
-	char *method = strtok(req_buffer, " ");
-    char *reqpath = strtok(NULL, " ");
 	int byteSent;
 
 	if(strcmp(reqpath, "/")==0){
@@ -145,7 +144,7 @@ void handle_connection(int fd){
 		if (!fp)
 		{
 			// If it doesn't exist, return 404
-			printf("File not found");
+			printf("File not found");	
 			char *res = "HTTP/1.1 404 Not Found\r\n\r\n"; // HTTP response
 			send(fd, res, strlen(res), 0);
 		}
@@ -184,47 +183,66 @@ void handle_connection(int fd){
 		send(fd, response, strlen(response), 0);
 	}
 	else if (strncmp(reqpath, "/files/", 7) == 0 && strcmp(method, "POST") == 0)
-	{
-        // Extract filename from the path
-        char *filename = reqpath + 7;
+	 {
+		method = strtok(NULL, "\r\n"); // HTTP 1.1
+		method = strtok(NULL, "\r\n"); // Content-Type
+		method = strtok(NULL, "\r\n"); // User-Agent
+		method = strtok(NULL, "\r\n"); // Accept: */*
+		method = strtok(NULL, "\r\n"); // Content-Length: X
 
-        // Find the headers
-        char *headers_end = strstr(req_buffer, "\r\n\r\n");
-        if (!headers_end) {
-            printf("Invalid request\n");
-            return;
-        }
+		char *contentLengthStr = strtok(method, " ");
+		contentLengthStr = strtok(NULL, " ");
 
-        // Move to the start of the body
-        char *body = headers_end + 4;
+		int contentLength = atoi(contentLengthStr);
 
-        // Extract Content-Length header value
-        char *content_length_str = strstr(req_buffer, "Content-Length: ");
-        if (!content_length_str) {
-            printf("Content-Length header not found\n");
-            return;
-        }
+		// Parse the file path
+		char *filename = strtok(reqpath, "/");
+		filename = strtok(NULL, "");
 
-        content_length_str += 16; // Move past "Content-Length: "
-        int content_length = atoi(content_length_str);
+		// Get the contents
+		content = strtok(content, "\r\n"); // Content: POST /files/dumpty_yikes_dooby_237 HTTP/1.1
+		printf("\n\n\nContent: %s\n\n\n", content);
+		content = strtok(NULL, "\r\n"); // Host: localhost:4221
+		content = strtok(NULL, "\r\n"); // User-Agent: curl/7.81.0
+		content = strtok(NULL, "\r\n"); // Accept: */*
+		content = strtok(NULL, "\r\n"); // Content-Length: 51
+		printf("\n\n\nContent: %s\n\n\n", content);
+		content = strtok(NULL, "\r\n"); // Content-Type: application/x-www-form-urlencoded
+		printf("\n\n\nContent: %s\n\n\n", content);
+		content = strtok(NULL, "\r\n"); // Content-Type: application/x-www-form-urlencoded
+		printf("\n\n\nContent: %s\n\n\n", content);
 
-        // Write content to file
-        FILE *fp = fopen(filename, "wb");
-        if (!fp) {
-            printf("File could not be opened\n");
-            char *res = "HTTP/1.1 500 Internal Server Error\r\n\r\n";
-            send(fd, res, strlen(res), 0);
-            return;
-        }
+		printf("\n---\nCreate a file %s with content length: %d\n\n %s\n---\n", filename, contentLength, content);
 
-        fwrite(body, 1, content_length, fp);
-        fclose(fp);
+		// Open the file in write binary mode
+		FILE *fp = fopen(filename, "wb");
+		if (!fp)
+		{
+			// If the file could not be created/opened
+			printf("File could not be opened");
+			char *res = "HTTP/1.1 404 Not Found\r\n\r\n"; // HTTP response
+			send(fd, res, strlen(res), 0);
+		}
+		else
+		{
+			printf("Opening file %s\n", filename);
+		}
 
-        // Send 201 Created response
-        char response[] = "HTTP/1.1 201 Created\r\n\r\n";
-        send(fd, response, strlen(response), 0);
-        printf("File %s created with content length: %d\n", filename, content_length);
-    }
+		// Write the contents
+		if (fwrite(content, 1, contentLength, fp) != contentLength)
+		{
+			printf("Error writing the data");
+		}
+
+		fclose(fp);
+
+		// Return contents
+		// Return contents
+		char response[1024];
+		sprintf(response, "HTTP/1.1 201 Created\r\nContent-Type: application/octet-stream\r\nContent-Length: %d\r\n\r\n%s", contentLength, content);
+		printf("Sending response: %s\n", response);
+		send(fd, response, strlen(response), 0);
+	}
 	else{
 		char response[] = "HTTP/1.1 404 Not Found\r\n\r\n";
 		send(fd,response, strlen(response),0);
